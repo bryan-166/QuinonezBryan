b5259362835b5686cdd6f818b7ce2be5fe612c30
Bryan-166 <bryan.quinonez.morales@utelvt.edu.ec>
2023-05-02 19:45:22 -0500

    cuarta actulizacion

--- head.php
+++ head.php
@@ -10,7 +10,7 @@
 <head>
  <title> Educaysoft </title>
 <meta charset="utf-8"/>
-	<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link href="images/Bryan.png" rel="shortcut icon" type="image/x-icon" />
 	<meta name="description" content="Biografía de Quiñonez Morales Bryan" />
 
 	<meta property="og:image" content="https://highkickesmeraldas.com/FrancisStalin/images/fotoperfil.png" />
